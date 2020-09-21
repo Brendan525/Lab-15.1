2f79d6b23eeb32de7eec3f86bfada335ed3a6631
Brendan Burch <44724846+Brendan525@users.noreply.github.com>
2020-09-21 17:44:25 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using Lab_15._1.Models;
+
+namespace Lab_15._1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Test()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://deckofcardsapi.com");
+            var shuffle = await client.GetAsync("/api/deck/new/shuffle/?deck_count=1");
+            var draw5 = await client.GetAsync("/api/deck/mfh4di5boakz/draw/?count=5");
+            var deckshuffle = await shuffle.Content.ReadAsStringAsync();
+            var new5 = await draw5.Content.ReadAsStringAsync();
+
+
+            return Content(deckshuffle);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Deck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_15._1.Models
+{
+    public class Deck
+    {
+
+        public bool success { get; set; }
+
+        public string deck_id { get; set; }
+
+        public string cards { get; set; }
+
+        public string code { get; set; }
+
+        public int value { get; set; }
+
+        public string image { get; set; }
+
+        public string suit { get; set; }
+
+        public int remaining { get; set; }
+
+        public bool shuffled { get; set; }
+    }
+}
